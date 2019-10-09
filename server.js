1829141b7cf6447542ffa9b2da2fabeb2282607b
X71012TE <mtemprano@viewnext.com>
2019-10-09 17:58:17 +0200

    activar CORS all domains

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express= require('express');
 const bodyParser = require('body-parser');
 const fs=require('fs');
+const cors=require('cors');
 
 const app= express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
